9d0595113569bb5cd90f4c3e62d3af22cf7e573f
Si-ghub <63141977+Si-ghub@users.noreply.github.com>
2021-09-14 20:46:22 +0300

    Authors: create, listAll

--- Author.js
+++ Author.js
@@ -1,9 +1,37 @@
 const Author = {};
 
+/**
+ * Knygos autoriaus irasymas i duomenu baze.
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes manipuliavimo metodus.
+ * @param {string} authorFirstname Autoriaus vardas.
+ * @param {string} authorLastname Autoriaus pavarde.
+ * @returns Tekstas, apibudinantis, koks autorius buvo irasytas i duomenu baze.
+ */
+
 Author.create = async (connection, authorFirstname, authorLastname) => {
+    const sql = 'INSERT INTO `authors`\
+                    (`id`, `firstname`, `lastname`)\
+                VALUES (NULL, "'+ authorFirstname + '", "' + authorLastname + '")';
+    const [rows] = await connection.execute(sql);
+    return `${authorFirstname} ${authorLastname} buvo sekmingai irasytas`;
 }
 
+/**
+ * Knygos autoriaus irasymas i duomenu baze.
+ * @param {Object} connection Objektas, su kuriuo kvieciame duombazes manipuliavimo metodus.
+ * @returns Visu autoriu, irasytu i duomenu baze, sarasas.
+ */
+
 Author.listAll = async (connection) => {
+    const sql = 'SELECT * FROM `authors`';
+    const [rows] = await connection.execute(sql);
+    let count = 0;
+    const authorsList = [];
+    for (let { firstname, lastname } of rows) {
+        authorsList.push(`${++count}. ${firstname} ${lastname}`);
+    };
+    const list = 'Autoriu sarasas:\n';
+    return list + authorsList.join('\n');
 }
 
 Author.findById = async (connection, authorId) => {
--- index.js
+++ index.js
@@ -13,8 +13,26 @@ app.init = async () => {
     });
 
     // LOGIC BELOW
-    await Author.create(conn, 'Vardenis', 'Pavardenis');
+    // create
+    console.log('');
+    const paula = await Author.create(conn, 'Paula', 'Paulaviciute')
+    const vardas = await Author.create(conn, 'Vardenis', 'Pavardenis');
+    const tomas = await Author.create(conn, 'Tomas', 'Manas');
+    const alessandro = await Author.create(conn, 'Alessandro', 'Baricco');
+    const michael = await Author.create(conn, 'Michael', 'Ondaatje');
+
+    console.log(paula);
+    console.log(vardas);
+    console.log(tomas);
+    console.log(alessandro);
+    console.log(michael);
+
+    // listAll
+    console.log('');
+    const authorsList = await Author.listAll(conn);
+    console.log(authorsList);
 }
+
 
 app.init();
 
